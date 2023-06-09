1e0c6d71c5e3ead7a8e85982b686ab4eb3ae0720
Ajay Dhuri <dhuriajay23@gmail.com>
2023-06-10 00:01:24 +0530

    Added product model.

---
+++ Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace OnlineShopAPI.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public int Quantity { get; set; }
+    }
+}
