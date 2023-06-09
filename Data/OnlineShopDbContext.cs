2321a7904146a9eafe468f65401138474dadc85c
Ajay Dhuri <dhuriajay23@gmail.com>
2023-06-10 01:03:59 +0530

    Added dbcontext file.

---
+++ Data/OnlineShopDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineShopAPI.Models;
+
+namespace OnlineShopAPI.Data
+{
+    public class OnlineShopDbContext : DbContext
+    {
+        public OnlineShopDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
