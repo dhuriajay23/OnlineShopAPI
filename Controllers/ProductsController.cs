83af449a4978cd1858b29ba5a6bc79a360710e8e
Ajay Dhuri <dhuriajay23@gmail.com>
2023-06-10 23:37:38 +0530

    Added endpoint to get all products.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿// Microsoft
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// Project
+using OnlineShopAPI.Data;
+
+namespace OnlineShopAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly OnlineShopDbContext _onlineShopDbContext;
+
+        public ProductsController(OnlineShopDbContext onlineShopDbContext)
+        {
+            _onlineShopDbContext = onlineShopDbContext;
+        }
+
+        [HttpGet("GetAllProducts")]
+        public async Task<IActionResult> Index()
+        {
+            return Ok(await _onlineShopDbContext.Products.ToListAsync());
+        }
+    }
+}
