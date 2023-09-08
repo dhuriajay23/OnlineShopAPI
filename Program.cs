cb3984f90af64152441cc8e844b3d95dbfceb930
Ajay Dhuri <dhuriajay23@gmail.com>
2023-09-08 17:24:52 +0530

    Enabled CORS.

--- Program.cs
+++ Program.cs
@@ -17,6 +17,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
 app.UseAuthorization();
 
 app.MapControllers();
